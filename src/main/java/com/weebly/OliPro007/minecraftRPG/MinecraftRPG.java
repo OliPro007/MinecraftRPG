c441954d30bd758ecc937625fad5c352d4229bdf
OliPro007 <OliPro007@gmail.com>
2014-06-23 15:10:52 -0400

    Basic changes for my mod (testing)

--- src/main/java/com/weebly/OliPro007/minecraftRPG/MinecraftRPG.java
+++ src/main/java/com/weebly/OliPro007/minecraftRPG/MinecraftRPG.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package com.weebly.OliPro007.minecraftRPG;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MinecraftRPG.MODID, version = MinecraftRPG.VERSION)
+public class MinecraftRPG
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "MinecraftRPG";
+    public static final String VERSION = "1.7.2-0.0.2";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
